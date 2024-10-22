2a19d9eabac1058e8a2f5b4e3a07d2108f0e3060
mani_m <manin@Manigandan>
2024-10-22 11:31:05 +0530

    Muthal Commit

---
+++ src/com/incubyte/spring/calc/SpringCalculator.java
@@ -0,0 +1,11 @@
+package com.incubyte.spring.calc;
+
+public class SpringCalculator {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Lawrence, \nWelcome to my Play House !");
+
+	}
+
+}
