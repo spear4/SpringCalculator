ede0716911b94c3008d9e555eedde28f60003723
manin <manin@Manigandan>
2024-10-22 16:25:11 +0530

    Irendam Commit (Adding jUnit)

---
+++ src/com/incubyte/string/calc/StringCalculator.java
@@ -0,0 +1,36 @@
+package com.incubyte.string.calc;
+
+import java.util.*;
+
+public class StringCalculator {
+	
+	public int add(String numbers) {
+		
+		int sum = 0;
+		
+		
+		
+		return sum;
+	}
+	
+	public String validate(String userInp) {
+		
+		String numbs = "";
+		
+		
+		return numbs;
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Lawrence, \nWelcome to my Play House !\n");
+		
+		Scanner inp = new Scanner(System.in);
+		
+		System.out.println("Provide Input of Numbers \nCan be separated by any delimiter []");
+		String userInp = inp.nextLine();
+		System.out.println(userInp);
+
+	}
+
+}
